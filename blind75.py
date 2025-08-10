05e1ad69d07f1861209e39274a349adc1856cbe8
+ <+>
2025-08-10 23:30:12 +0300

    twosum2nd soln

--- blind75.py
+++ blind75.py
@@ -36,7 +36,7 @@ class Solution:
 # You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
 # Return the answer with the smaller index first.
 from typing import List #day 4
-
+#hash map solution(one pass)
 class Solution:
     def twoSum(self, nums: List[int], target:int) -> List[int]:
         seen = {}
@@ -46,3 +46,15 @@ class Solution:
                 return [seen[complement], i]
             seen[num] = i
 
+#hash map solution(two pass)
+class Solution:
+    def twoSum(seld, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i, n in enumerate(nums):
+            seen[n] = i
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in seen and seen[diff] != i:
+                return [i, seen[diff]]
