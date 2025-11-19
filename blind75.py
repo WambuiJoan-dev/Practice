a92141492613f44e5273182911c1619649cde34f
+ <+>
2025-11-19 06:17:09 +0300

    2nd stockprice

--- blind75.py
+++ blind75.py
@@ -106,3 +106,18 @@ class Solution:
             if current_profit > max_profit:
                 max_profit = current_profit #update max profit if current profit is higher
         return max_profit
+    
+    #or
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r=0, 1
+        maxP = 0
+
+        while r<len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
